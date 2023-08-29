b453f98b85f575718cdae964829ab3c078319695
mengdaoshizhongxinyang <programmercyk@gmail.com>
2023-08-29 16:19:40 +0800

    chore: update config

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,12 @@
-import { swc } from "rollup-plugin-swc3";
 import { defineConfig } from 'vite'
 import { VueTSPropsPlugin } from "vue-ts-props-plugin-vite";
 import VueJsx from "@vitejs/plugin-vue-jsx";
+import Inspect from 'vite-plugin-inspect'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    Inspect(),
     VueTSPropsPlugin(),
-    VueJsx(),
-    swc({
-      include: /\.[mc]?[jt]sx?$/,
-      exclude: /node_modules/,
-      tsconfig: 'tsconfig.json',
-      jsc: {
-        baseUrl:"/",
-        parser: {
-          "syntax": "ecmascript",
-          "jsx": true
-        },
-        experimental: {
-          plugins: []
-        }
-      }
-    })]
-
-    
+    VueJsx()
+  ]
 })
